920fa54c8719045052273a4875272a231ad7b044
Vangod <vachri.attala@gmail.com>
2022-03-19 00:50:42 +0700

    fix overlay UI to be reusable

--- Assets/UI_Overlay.cs
+++ Assets/UI_Overlay.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class UI_Overlay : MonoBehaviour, IPointerClickHandler {
-  public UI_Inventory inv;
+  public GameObject parent;
 
   public void OnPointerClick(PointerEventData eventData) {
-    inv.CloseInventory();
+    parent.SetActive(false);
   }
 }
