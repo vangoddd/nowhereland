e11174bbc13fa7a6501654a9e682b1c3ef1ef987
Vangod <vachri.attala@gmail.com>
2022-03-21 18:48:06 +0700

    add placeable item SO

---
+++ Assets/Scripts/ScriptableObject/Placeable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Item/Placeable item")]
+public class Placeable : ItemData {
+  public GameObject worldObjectPrefab;
+
+}
--- Assets/Scripts/WorldObject/Placeable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Placeable : Destroyable {
-  public virtual void PlaceObject() {
-    Debug.Log("Placing " + gameObject.name);
-  }
-}
