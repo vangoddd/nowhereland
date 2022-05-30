dac6e3e8e56da6c8cd6f424452677249e1b6b384
Vangod <vachri.attala@gmail.com>
2022-05-30 17:33:25 +0700

    fixed item display on player

--- Assets/EquippedItem.cs
+++ Assets/EquippedItem.cs
@@ -37,14 +37,35 @@ public class EquippedItem : MonoBehaviour {
     _animator.SetFloat("moveX", moveDir.x);
     _animator.SetFloat("moveY", moveDir.y);
 
+    setFacing(moveDir);
+
     if (facing == 0) {
       transform.localPosition = new Vector3(-0.4f, 0.3f, 0f);
+      sr.flipX = true;
+      sr.sortingOrder = -1;
     } else if (facing == 1) {
       transform.localPosition = new Vector3(0.2f, 0.3f, 0f);
+      sr.flipX = false;
+      sr.sortingOrder = 1;
     } else if (facing == 2) {
       transform.localPosition = new Vector3(0.4f, 0.3f, 0f);
+      sr.flipX = false;
+      sr.sortingOrder = 1;
     } else if (facing == 3) {
       transform.localPosition = new Vector3(-0.2f, 0.3f, 0f);
+      sr.flipX = true;
+      sr.sortingOrder = -1;
+    }
+  }
+
+  void setFacing(Vector2 moveDir) {
+    //going on x axis
+    if (moveDir.x * moveDir.x > moveDir.y * moveDir.y) {
+      if (moveDir.x >= 0) facing = 1;
+      else facing = 3;
+    } else { //going on y axis
+      if (moveDir.y >= 0) facing = 0;
+      else facing = 2;
     }
   }
 }
